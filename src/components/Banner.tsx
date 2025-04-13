715829ac4a468949d2302b32273406dff71b09a2
loipv03 <phanvanloi1522003@gmail.com>
2025-04-14 01:03:18 +0700

    Add UI Banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Banner() {
+  return (
+    <div className="w-full px-36 py-10 bg-gray-100 rounded-2xl h-[300px] m-auto grid grid-cols-[65%_auto] gap-x-2 justify-items-end">
+      <div className="w-full grid grid-rows-[max-content_auto] gap-y-3 m-auto">
+        <h1 className="w-full capitalize text[#333] text-4xl leading-12 font-bold">
+          <span className="text-orange-400">Chọn Đúng Trường</span> Vạn Dặm
+          Thành Công Trên Con Đường Ước Mơ
+        </h1>
+        <p className="w-[450px] capitalize text-[#6b6b6b] text-sm font-normal leading-[24px]">
+          Tìm hiểu về các yếu tố quan trọng như chất lượng đào tạo, cơ sở vật
+          chất, đội ngũ giảng viên và môi trường học tập.
+        </p>
+      </div>
+      <div className="relative w-full grid place-items-center">
+        <Image
+          src={"/banner.png"}
+          alt="banner"
+          fill
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
-    <header className="sticky select-none  shadow top-0 w-full h-16 lg:px-24 grid grid-cols-[20%_auto_20%_max-content] place-items-center gap-x-2">
+    <header className="mb-5 sticky z-50 bg-white select-none shadow top-0 w-full h-16 lg:px-24 grid grid-cols-[20%_auto_20%_max-content] place-items-center gap-x-2">
       <div className="w-full max-h-full">
         <Link
           href="/"
